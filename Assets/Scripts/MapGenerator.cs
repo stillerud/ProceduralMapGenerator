c03657340083a2c815f76bff8bd0e26ae75d2325
stillerud <stillerud@hotmail.com>
2017-01-03 15:11:07 +0100

    Colour - E04
    In this commit we add the ability to divide the Noise Map into terrain
    regions and add color to those regions.
    
    We also moved part of the map display into a separate script that
    handles the texture generation so that we can easily display different
    maps.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Map generator class
 public class MapGenerator : MonoBehaviour {
 
+	public enum DrawMode {NoiseMap, ColourMap};
+	public DrawMode drawMode;
+
+	// Public variables to control the map generation and noise map
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -18,13 +23,35 @@ public class MapGenerator : MonoBehaviour {
 
 	public bool autoUpdate;
 
+	public TerrainType[] regions;
+
+	// Method that generate the map by a noise map and display it
 	public void GenerateMap() {
 		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
+		// Loop through each terrain type region and assign colour to our colour map
+		Color[] colourmMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colourmMap [y * mapWidth + x] = regions [i].colour;
+						break;
+					}
+				}
+			}
+		}
+
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		display.DrawNoiseMap (noiseMap);
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap(noiseMap));
+		} else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture (TextureGenerator.TextureFromColourMap (colourmMap, mapWidth, mapHeight));
+		}
 	}
 
+	// Clamps the public variables
 	void OnValidate() {
 		if (mapWidth < 1) {
 			mapWidth = 1;
@@ -40,3 +67,11 @@ public class MapGenerator : MonoBehaviour {
 		}
 	}
 }
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color colour;
+}
+
