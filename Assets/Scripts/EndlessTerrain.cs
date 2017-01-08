192723b16abb24417d17100e3534fcc846e7e414
stillerud <stillerud@hotmail.com>
2017-01-08 19:04:15 +0000

    LOD switching - E09
    - Further Implemented threading
    - Added a system to manage switching out mesh level of detail based on
    user customized distances.
    - added summaries and comments
    - the terrain chunks get noise map data from its position.

--- Assets/Scripts/EndlessTerrain.cs
+++ Assets/Scripts/EndlessTerrain.cs
@@ -7,11 +7,16 @@ using UnityEngine;
 /// </summary>
 public class EndlessTerrain : MonoBehaviour {
 
-	public const float maxViewDist = 450;
+	const float viewerMoveThresholdForChunkUpdate = 25f;
+	const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
+
+	public LODInfo[] detailLevels; 
+	public static float maxViewDst;
 	public Transform viewer;
 	public Material mapMaterial;
 
 	public static Vector2 viewerPosition;
+	Vector2 viewerPositionOld;
 	static MapGenerator mapGenerator;
 	int chunkSize;
 	int chunksVisibleInViewDst;
@@ -21,18 +26,29 @@ public class EndlessTerrain : MonoBehaviour {
 
 	void Start() {
 		mapGenerator = FindObjectOfType<MapGenerator> ();
+
+		maxViewDst = detailLevels [detailLevels.Length - 1].visibleDstThreshold;
 		chunkSize = MapGenerator.mapChunkSize - 1;
-		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDist / chunkSize);
+		chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
 	
+		UpdateVisibleChunks ();
 	}
 
 	void Update() {
 		viewerPosition = new Vector2 (viewer.position.x, viewer.position.z);
-		UpdateVisibleChunks ();
+
+		if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate) {
+			viewerPositionOld = viewerPosition;
+			UpdateVisibleChunks ();
+		}
 	}
 
+	/// <summary>
+	/// Updates the visible chunks.
+	/// </summary>
 	void UpdateVisibleChunks() {
 
+		// Hide all terrain chunks and clear the list of visible chunks from last update
 		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) {
 			terrainChunksVisibleLastUpdate [i].SetVisible (false);
 		}
@@ -41,24 +57,26 @@ public class EndlessTerrain : MonoBehaviour {
 		int currentChunkCoordX = Mathf.RoundToInt (viewerPosition.x / chunkSize);
 		int currentChunkCoordY = Mathf.RoundToInt (viewerPosition.y / chunkSize);
 
+		// Loop through the visible chunks based on view distance and set their coordinates
 		for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++) {
 			for (int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++) {
 				Vector2 viewedChunkCoord = new Vector2 (currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
 
+				// If chunk is allready generated, add it to the update dictionary
 				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
 					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk ();
 					if (terrainChunkDictionary [viewedChunkCoord].IsVisible ()) {
 						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
 					}
 				} else { // create new terrain chunk
-					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, transform, mapMaterial));
+					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
 				}
 			}
 		}
 	}
 
 	/// <summary>
-	/// Terrain chunk struct keeps track of the terrain mesh, its position, bounds as well as updating and hiding it.
+	/// Terrain chunk class keeps track of the terrain mesh, its position and level of detail.
 	/// </summary>
 	public class TerrainChunk {
 
@@ -66,12 +84,27 @@ public class EndlessTerrain : MonoBehaviour {
 		Vector2 position;
 		Bounds bounds;
 
-		MapData mapData;
-
 		MeshRenderer meshRenderer;
 		MeshFilter meshFilter;
 
-		public TerrainChunk(Vector2 coord, int size, Transform parent, Material material) {
+		LODInfo[] detailLevels;
+		LODMesh[] lodMeshes;
+
+		MapData mapData;
+		bool mapDataReceived;
+		int previousLODIndex = -1;
+
+		/// <summary>
+		/// Initializes a new instance of the TerrainChunk class.
+		/// </summary>
+		/// <param name="coord">Coordinate position amongst other terrain chunks.</param>
+		/// <param name="size">Size of the chunk in world units.</param>
+		/// <param name="detailLevels">List with level of details.</param>
+		/// <param name="parent">Parent transform.</param>
+		/// <param name="material">Material to be used.</param>
+		public TerrainChunk(Vector2 coord, int size, LODInfo[] detailLevels, Transform parent, Material material) {
+			this.detailLevels = detailLevels;
+
 			position = coord * size;
 			bounds = new Bounds(position, Vector2.one * size);
 			Vector3 positionV3 = new Vector3(position.x, 0, position.y);
@@ -85,31 +118,129 @@ public class EndlessTerrain : MonoBehaviour {
 			meshObject.transform.parent = parent;
 			SetVisible(false);
 
+			lodMeshes = new LODMesh[detailLevels.Length];
+			for (int i = 0; i < detailLevels.Length; i++) {
+				lodMeshes[i] = new LODMesh(detailLevels[i].lod, UpdateTerrainChunk);
+			}
+
 			// Ask for map data by threading.
-			mapGenerator.RequestMapData(OnMapDataReceived);
+			mapGenerator.RequestMapData(position, OnMapDataReceived);
 		}
-
-		// Callback method: We've received the map data so we can now ask for the mesh data by threading
+			
+		/// <summary>
+		/// Raises the map data received event.
+		/// </summary>
+		/// <param name="mapData">Map data.</param>
 		void OnMapDataReceived(MapData mapData) {
-			mapGenerator.RequestMeshData (mapData, OnMeshDataReceived);
+			this.mapData = mapData;
+			mapDataReceived = true;
+
+			Texture2D texture = TextureGenerator.TextureFromColourMap (mapData.colourMap, MapGenerator.mapChunkSize, MapGenerator.mapChunkSize);
+			meshRenderer.material.mainTexture = texture;
+
+			UpdateTerrainChunk ();
 		}
 
-		// Callback method: We've recieved the meshdata so we can now create the mesh.
-		void OnMeshDataReceived(MeshData meshData) {
-			meshFilter.mesh = meshData.CreateMesh();
-		}
-
+		/// <summary>
+		/// Updates the terrain chunks visibility and level of detail.
+		/// </summary>
 		public void UpdateTerrainChunk() {
-			float viewerDstFroNearestEdge = Mathf.Sqrt (bounds.SqrDistance (viewerPosition));
-			bool visible = viewerDstFroNearestEdge <= maxViewDist;
-			SetVisible (visible);
+			if(mapDataReceived) {
+				float viewerDstFromNearestEdge = Mathf.Sqrt (bounds.SqrDistance (viewerPosition));
+				bool visible = viewerDstFromNearestEdge <= maxViewDst;
+
+				if (visible) {
+					int lodIndex = 0;
+
+					// Find the correct detail level that this chunk should have based on distance to viewer
+					for (int i = 0; i < detailLevels.Length - 1; i++) { 
+						if (viewerDstFromNearestEdge > detailLevels [i].visibleDstThreshold) {
+							lodIndex = i + 1;
+						} else {
+							break;
+						}
+					}
+
+					// We need to change the level of detail of the mesh
+					if (lodIndex != previousLODIndex) {
+						LODMesh lodMesh = lodMeshes [lodIndex];
+						if (lodMesh.hasMesh) {
+							previousLODIndex = lodIndex;
+							meshFilter.mesh = lodMesh.mesh; // Mesh exists, so draw it
+						} else if (!lodMesh.hasRequestedMesh) {
+							lodMesh.RequestMesh (mapData); // Request mesh
+						}
+					}
+				}
+
+				SetVisible (visible);
+			}
 		}
 
+		/// <summary>
+		/// Hides the terrain chunk
+		/// </summary>
+		/// <param name="visible">Is visible if set to <c>true</c>.</param>
 		public void SetVisible(bool visible) {
 			meshObject.SetActive (visible);
-		} 
+		}
+		/// <summary>
+		/// Determines whether this instance is visible.
+		/// </summary>
+		/// <returns><c>true</c> if this instance is visible; otherwise, <c>false</c>.</returns>
 		public bool IsVisible() {
 			return meshObject.activeSelf;
 		}
+	}
+
+	/// <summary>
+	/// Level of detail mesh class. Contains the mesh as well as level of detail info.
+	/// </summary>
+	class LODMesh {
+		public Mesh mesh;
+		public bool hasRequestedMesh;
+		public bool hasMesh;
+		int lod;
+		System.Action updateCallback;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LODMesh"/> class.
+		/// </summary>
+		/// <param name="lod">Level of detail.</param>
+		/// <param name="updateCallback">Update callback method.</param>
+		public LODMesh(int lod, System.Action updateCallback) {
+			this.lod = lod;
+			this.updateCallback = updateCallback;
+		}
+
+		// 
+		/// <summary>
+		/// Raises the mesh data received event.
+		/// </summary>
+		/// <param name="meshData">Mesh data.</param>
+		void OnMeshDataReceived(MeshData meshData) {
+			mesh = meshData.CreateMesh ();
+			hasMesh = true;
+
+			updateCallback ();
+		}
+			
+		/// <summary>
+		/// Requests the mesh from map generator
+		/// </summary>
+		/// <param name="mapData">Map data to be used.</param>
+		public void RequestMesh(MapData mapData) {
+			hasRequestedMesh = true;
+			mapGenerator.RequestMeshData (mapData, lod, OnMeshDataReceived);
+		}
+	}
+		
+	/// <summary>
+	/// Struct for user customizable list of level of details and their respective distance thresholds.
+	/// </summary>
+	[System.Serializable]
+	public struct LODInfo {
+		public int lod;
+		public float visibleDstThreshold;
 	}
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -15,7 +15,7 @@ public class MapGenerator : MonoBehaviour {
 	// Public variables to control the map generation and noise map
 	public const int mapChunkSize = 241; // max
 	[Range(0,6)]
-	public int levelOfDetail;
+	public int editorPreviewLOD;
 	public float noiseScale;
 
 	public int octaves;
@@ -41,7 +41,7 @@ public class MapGenerator : MonoBehaviour {
 	/// Chooses what to show in editor based on the DrawMode enumorator.
 	/// </summary>
 	public void DrawMapInEditor() {
-		MapData mapData = GenerateMapData ();
+		MapData mapData = GenerateMapData (Vector2.zero);
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
 		if (drawMode == DrawMode.NoiseMap) {
@@ -49,7 +49,7 @@ public class MapGenerator : MonoBehaviour {
 		} else if (drawMode == DrawMode.ColourMap) {
 			display.DrawTexture (TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
 		} else if (drawMode == DrawMode.Mesh) {
-			display.DrawMesh (MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
 		}
 	}
 
@@ -57,9 +57,9 @@ public class MapGenerator : MonoBehaviour {
 	/// Method that requests map data by spawning a new thread.
 	/// </summary>
 	/// <param name="callback">Callback.</param>
-	public void RequestMapData(Action<MapData> callback) {
+	public void RequestMapData(Vector2 centre, Action<MapData> callback) {
 		ThreadStart threadStart = delegate {
-			MapDataThread (callback);
+			MapDataThread (centre, callback);
 		};
 
 		new Thread (threadStart).Start ();
@@ -69,8 +69,8 @@ public class MapGenerator : MonoBehaviour {
 	/// Thread that generates map data and adds it to the callback queue.
 	/// </summary>
 	/// <param name="callback">Callback.</param>
-	void MapDataThread(Action<MapData> callback) {
-		MapData mapData = GenerateMapData ();
+	void MapDataThread(Vector2 centre, Action<MapData> callback) {
+		MapData mapData = GenerateMapData (centre);
 		lock (mapDataThreadInfoQueue) {
 			mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<MapData> (callback, mapData));
 		}
@@ -80,29 +80,31 @@ public class MapGenerator : MonoBehaviour {
 	/// Method that requests mesh data by spawning a new thread with the map data.
 	/// </summary>
 	/// <param name="mapData">Map data.</param>
+	/// <param name="lod">Level Of detail.</param>
 	/// <param name="callback">Callback.</param>
-	public void RequestMeshData(MapData mapData, Action<MeshData> callback) {
+	public void RequestMeshData(MapData mapData,int lod, Action<MeshData> callback) {
 		ThreadStart threadstart = delegate {
-			MeshDataThread (mapData, callback);
+			MeshDataThread (mapData, lod, callback);
 		};
 
 		new Thread (threadstart).Start ();
 	}
 
+
 	/// <summary>
 	/// Thread that generates mesh data and adds it to the callback queue.
 	/// </summary>
 	/// <param name="mapData">Map data.</param>
+	/// <param name="lod">Level of detail.</param>
 	/// <param name="callback">Callback.</param>
-	void MeshDataThread(MapData mapData, Action<MeshData> callback) {
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail);
+	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback) {
+		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
 		lock (meshDataThreadInfoQueue) {
 			meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData>(callback, meshData));
 		}
 	}
 
 	void  Update() {
-
 		// Loop through any items in our map callback queue and ask them to call their callback method with map data.
 		if (mapDataThreadInfoQueue.Count > 0) {
 			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++) {
@@ -124,8 +126,8 @@ public class MapGenerator : MonoBehaviour {
 	/// Method that generate the map by a perling noise map.
 	/// </summary>
 	/// <returns>Returns a MapData struct containing the generated noise and colour map.</returns>
-	MapData GenerateMapData() {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistence, lacunarity, offset);
+	MapData GenerateMapData(Vector2 centre) {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistence, lacunarity, centre + offset);
 
 		// Loop through each terrain type region and assign colour to our colour map
 		Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
